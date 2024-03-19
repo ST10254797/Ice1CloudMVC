330ebdcc16ca797b2b2b6e0cfe7f73d67615748e
ST10254797 <128587735+ST10254797@users.noreply.github.com>
2024-03-19 22:01:57 +0200

    Add files via upload

---
+++ Controllers/ContactUs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmployeeApp.Data;
+using EmployeeApp.Models;
+
+namespace EmployeeApp.Controllers
+{
+	public class ContactUs : Controller
+	{
+		public IActionResult Index()
+		{
+			return View("ContactUs");
+		}
+	}
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using EmployeeApp.Data;
+using System;
+using System.Linq;
+namespace EmployeeApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new EmployeeAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<EmployeeAppContext>>()))
+            {
+                // Look for any movies.
+                if (context.Employee.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Employee.AddRange(
+                    new Employee
+                    {
+                        Name = "Devesh",
+                        Department = "IT",
+                        Salary = 600000
+                    },
+                    new Employee
+                    {
+                        Name = "Mershen",
+                        Department = "Accounting",
+                        Salary = 500000
+                    },
+                    new Employee
+                    {
+                        Name = "Caden",
+                        Department = "Law",
+                        Salary = 1100000
+
+                    },
+                    new Employee
+                    {
+
+                        Name = "Preshen",
+                        Department = "Health",
+                        Salary = 650000
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
